f1f26bd25d65fdeddd575f8ed04fbb3b9a771255
Fred Buchanan <oscillator.b@gmail.com>
2025-02-09 15:05:32 -0500

    Added form submission

--- votes-like-my-fave/src/pages/index.tsx
+++ votes-like-my-fave/src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { CURRENT_CONGRESS, LegislatorSummary, list_legislators_by_congress } from "@/lib/database";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import React from "react";
+import React, { FormEvent } from "react";
 
 const NUM_SEARCH_RESULTS = 3;
 
 export default function Home({ legislators }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const [searchResults, setSearchResult] = React.useState(legislators.slice(0,NUM_SEARCH_RESULTS));
+  const [searchResults, setSearchResult] = React.useState(legislators.slice(0, NUM_SEARCH_RESULTS));
 
   const onSearchChange = (content: string) => {
     const normalizedContent = content.toLowerCase();
@@ -14,13 +14,17 @@ export default function Home({ legislators }: InferGetStaticPropsType<typeof get
       return (l.family_name.toLowerCase().indexOf(normalizedContent) != -1) || (l.given_name.toLowerCase().indexOf(normalizedContent) != -1);
     })
 
-    setSearchResult(candidateResults.slice(0,NUM_SEARCH_RESULTS));
+    setSearchResult(candidateResults.slice(0, NUM_SEARCH_RESULTS));
   };
+
+  const onSubmit = (legislator: LegislatorSummary | undefined) => {
+    console.log(legislator);
+  }
 
   return (
     <div>
       <div className="flex justify-center h-screen bg-gray-100">
-        <SearchBox results={searchResults} onSearchChange={onSearchChange} />
+        <SearchBox results={searchResults} onSearchChange={onSearchChange} onSubmit={onSubmit}/>
       </div>
     </div>
   );
@@ -33,28 +37,38 @@ export const getStaticProps = (async () => {
 
 interface SearchBoxProps {
   results: LegislatorSummary[],
-  onSearchChange: (text: string) => void
+  onSearchChange: (text: string) => void,
+  onSubmit: (legislator: LegislatorSummary | undefined) => void
 }
 
-function SearchBox({ results, onSearchChange }: SearchBoxProps) {
-  "use client";
-  const container: React.Ref<HTMLDivElement|null> = React.useRef(null);
+function SearchBox({ results, onSearchChange, onSubmit }: SearchBoxProps) {
+  const container: React.Ref<HTMLFormElement | null> = React.useRef(null);
   const [focused, setFocus] = React.useState(false);
 
-  const onFocus = () => setFocus(container.current!.contains(document.activeElement));
-  const onBlur = () => setFocus(container.current!.contains(document.activeElement));
+  const onFocus = () => setFocus(true);
+  const onBlur = () => setFocus(true);
+
+  const onSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Submit");
+    onSubmit(results.at(0));
+  };
+
+  const onResultSelect = (l: LegislatorSummary) => {
+    onSubmit(l);
+  }
 
   return (
     <div className="relative w-full max-w-xl pt-40">
-      <div className="relative shadow-md" ref={container} onFocus={onFocus} onBlur={onBlur}>
+      <form className="relative shadow-md" ref={container} onFocus={onFocus} onBlur={onBlur} onSubmit={onSubmitHandler}>
         <input
           type="text"
           placeholder="Search..."
           className={"w-full p-3 pl-5 text-lg border focus:outline-none " + (focused ? "rounded-t-md" : "rounded-md")}
           onChange={(e) => onSearchChange(e.target.value)}
-          />
+        />
 
-        <button className="absolute right-3 top-1/2 transform -translate-y-1/2 bg-blue-500 p-2 rounded-full text-white hover:ring-2 hover:scale-110 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent">
+        <button type="submit" name="default_submit" value={results.at(0)?.bioguide_id} className="absolute right-3 top-1/2 transform -translate-y-1/2 bg-blue-500 p-2 rounded-full text-white hover:ring-2 hover:scale-110 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent">
           <MagnifyingGlassIcon className="w-6 h-6" />
         </button>
 
@@ -62,23 +76,28 @@ function SearchBox({ results, onSearchChange }: SearchBoxProps) {
           <div className="absolute w-full border  bg-white rounded-b-md">
             <ul className="divide-y divide-gray-200">
               {results.map(r =>
-                <SearchResult result={r} />
+                <SearchResult key={r.bioguide_id} result={r} onResultSelect={onResultSelect} />
               )}
             </ul>
           </div> : null
         }
-      </div>
+      </form>
     </div>
   );
 }
 
 interface SearchResultProps {
-  result: LegislatorSummary
+  result: LegislatorSummary,
+  onResultSelect: (legislator: LegislatorSummary) => void
 }
 
-function SearchResult({ result }: SearchResultProps) {
-  return <li key={result.bioguide_id} className="hover:bg-gray-100 cursor-pointer">
-    <button className="w-full h-full p-3 pl-5 text-left">{result.family_name}, {result.given_name} ({result.party}-{result.state})</button>
+function SearchResult({ result, onResultSelect}: SearchResultProps) {
+  const onClick = () => {
+    onResultSelect(result);
+  };
+
+  return <li className="hover:bg-gray-100 cursor-pointer">
+    <button type="button" className="w-full h-full p-3 pl-5 text-left" onClick={onClick}>{result.family_name}, {result.given_name} ({result.party}-{result.state})</button>
   </li>;
 }
 
