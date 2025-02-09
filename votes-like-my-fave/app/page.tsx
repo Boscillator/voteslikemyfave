f66591dd846f34d2c2b4a02e20d129f886e44d3e
Fred Buchanan <oscillator.b@gmail.com>
2025-02-09 12:47:07 -0500

    Got database connection set up

--- votes-like-my-fave/app/page.tsx
+++ votes-like-my-fave/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import { CURRENT_CONGRESS, LegislatorSummary, list_legislators_by_congress } from "@/lib/database";
 
-export default function Home() {
+type Props = {
+  legislators: LegislatorSummary[]
+}
+
+export default async function Home() {
+  const legislators = await list_legislators_by_congress(CURRENT_CONGRESS);
   return (
     <div>
       <h1>Hello World</h1>
+      <ul>{ legislators.map(l => <li key={l.bioguide_id}>{l.family_name}</li>) }</ul>
     </div>
   );
 }
